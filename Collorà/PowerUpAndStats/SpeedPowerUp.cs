bbeb6bb2752f09205b3e7a8b6f51e03d5fedd7e2
Simone Collorà <simone.collora@studio.unibo.it>
2023-04-20 12:19:39 +0200

    Fix

--- Collorà/PowerUpAndStats/SpeedPowerUp.cs
+++ Collorà/PowerUpAndStats/SpeedPowerUp.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-
 namespace PowerUpAndStats
 {
     public class SpeedPowerUp : PowerUp
--- Collorà/PowerUpAndStatsTest/PowerUpTest.cs
+++ Collorà/PowerUpAndStatsTest/PowerUpTest.cs
@@ -1,37 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PowerUpAndStats;
 
-namespace PowerUpAndStatsTest;
-
-[TestClass]
-public class PowerUpTest
+namespace PowerUpAndStatsTest
 {
-    [TestMethod]
-    public async Task AttackPowerUpTest()
+    [TestClass]
+    public class PowerUpTest
     {
-        PlayerStats stats = new PlayerStats(4, 5, 6);
-        PowerUp powup = new AttackPowerUp();
-        double expectedValue = stats.Attack + 2;
-        double oldvalue = stats.Attack;
-        powup.StatPowerUp(stats);
-        Assert.AreEqual(expectedValue, stats.Attack);
-        await Task.Delay(8000); //additional second added because the task is not completed always in 7000 ms
-        Assert.AreEqual(oldvalue, stats.Attack);
+        [TestMethod]
+        public async Task AttackPowerUpTest()
+        {
+            PlayerStats stats = new PlayerStats(4, 5, 6);
+            PowerUp powup = new AttackPowerUp();
+            double expectedValue = stats.Attack + 2;
+            double oldvalue = stats.Attack;
+            powup.StatPowerUp(stats);
+            Assert.AreEqual(expectedValue, stats.Attack);
+            await Task.Delay(8000); //additional second added because the task is not completed always in 7000 ms
+            Assert.AreEqual(oldvalue, stats.Attack);
+        }
+
+        [TestMethod]
+        public async Task SpeedPowerUpTest()
+        {
+            PlayerStats stats = new PlayerStats(4, 5, 6);
+            PowerUp powup = new SpeedPowerUp();
+            double expectedValue = stats.Speed + 2;
+            double oldvalue = stats.Speed;
+            powup.StatPowerUp(stats);
+            Assert.AreEqual(expectedValue, stats.Speed);
+            await Task.Delay(8000);
+            Assert.AreEqual(oldvalue, stats.Speed);
+        }
     }
-
-    [TestMethod]
-    public async Task SpeedPowerUpTest()
-    {
-        PlayerStats stats = new PlayerStats(4, 5, 6);
-        PowerUp powup = new SpeedPowerUp();
-        double expectedValue = stats.Speed + 2;
-        double oldvalue = stats.Speed;
-        powup.StatPowerUp(stats);
-        Assert.AreEqual(expectedValue, stats.Speed);
-        await Task.Delay(8000);
-        Assert.AreEqual(oldvalue, stats.Speed);
-    }
-
-
-
 }
