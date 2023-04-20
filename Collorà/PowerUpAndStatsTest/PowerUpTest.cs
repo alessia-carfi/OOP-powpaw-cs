10be81500a830e3adf4f40735afe5d2a344ce2cd
Simone Collorà <simone.collora@studio.unibo.it>
2023-04-20 11:52:36 +0200

    From Xunit to Microsoft Test

--- Collorà/PowerUpAndStatsTest/PowerUpTest.cs
+++ Collorà/PowerUpAndStatsTest/PowerUpTest.cs
@@ -1,33 +1,35 @@
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerUpAndStats;
 
-namespace PowerUpAndStats;
+namespace PowerUpAndStatsTest;
 
+[TestClass]
 public class PowerUpTest
 {
-    [Fact]
-    public static async Task AttackPowerUpTest()
+    [TestMethod]
+    public async Task AttackPowerUpTest()
     {
         PlayerStats stats = new PlayerStats(4, 5, 6);
         PowerUp powup = new AttackPowerUp();
         double expectedValue = stats.Attack + 2;
         double oldvalue = stats.Attack;
         powup.StatPowerUp(stats);
-        Assert.Equal(expectedValue, stats.Attack);
+        Assert.AreEqual(expectedValue, stats.Attack);
         await Task.Delay(8000); //additional second added because the task is not completed always in 7000 ms
-        Assert.Equal(oldvalue, stats.Attack);
+        Assert.AreEqual(oldvalue, stats.Attack);
     }
 
-    [Fact]
-    public static async Task SpeedPowerUpTest()
+    [TestMethod]
+    public async Task SpeedPowerUpTest()
     {
         PlayerStats stats = new PlayerStats(4, 5, 6);
         PowerUp powup = new SpeedPowerUp();
         double expectedValue = stats.Speed + 2;
         double oldvalue = stats.Speed;
         powup.StatPowerUp(stats);
-        Assert.Equal(expectedValue, stats.Speed);
+        Assert.AreEqual(expectedValue, stats.Speed);
         await Task.Delay(8000);
-        Assert.Equal(oldvalue, stats.Speed);
+        Assert.AreEqual(oldvalue, stats.Speed);
     }
 
 
